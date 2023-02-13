78ba59f0b2ca7eff5a74ae710337262276f129bc
Davi Patricio <davipatricio2006@gmail.com>
2023-02-13 23:18:57 +0000

    fix build

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,6 +17,8 @@ const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default function Home() {
+  if (typeof window === "undefined") return <></>;
+
   const router = useRouter();
   const account = useContext(AccountContext);
 
