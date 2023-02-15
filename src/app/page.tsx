9a76a4ac8e70703c9feff3d5c2d65e0faa205f3d
Davi Patricio <davipatricio2006@gmail.com>
2023-02-15 01:30:46 +0000

    make components dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import Feed from "@/components/Feed";
-import Navbar from "@/components/Navbar";
-import ValidUserCheck from "@/components/ValidUserCheck";
+import dynamic from "next/dynamic";
+
+const Feed = dynamic(() => import("@/components/Feed"), { ssr: false });
+const Navbar = dynamic(() => import("@/components/Navbar"), { ssr: false });
+const ValidUserCheck = dynamic(() => import("@/components/ValidUserCheck"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
