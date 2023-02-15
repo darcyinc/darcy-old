de949b5003a2fa8fa9cd3cef95c8113455b44519
Davi Patricio <davipatricio2006@gmail.com>
2023-02-15 01:13:54 +0000

    remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,6 @@ import Navbar from "@/components/Navbar";
 import ValidUserCheck from "@/components/ValidUserCheck";
 
 export default function Home() {
-  // const account = useContext(AccountContext);
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const currentToken = localStorage.getItem("token") ?? account.data?.token;
-  //   if (!currentToken) {
-  //     router.replace("/login");
-  //     return;
-  //   }
-
-  //   getUserFromToken(currentToken)
-  //     .then(account.setData)
-  //     .catch(() => {
-  //       localStorage.removeItem("token");
-  //       account.setData({});
-  //       return router.replace("/login");
-  //     });
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps -- Prevent infinite re-render
-  // }, []);
-
   return (
     <>
       <ValidUserCheck redirectToIfNotLogged="/login" />
