2ca8dbba65a9076742493cd69fcf4e09fbf3fb6c
Davi Patricio <davipatricio2006@gmail.com>
2023-02-15 21:00:34 +0000

    remove head.tsx comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,10 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
