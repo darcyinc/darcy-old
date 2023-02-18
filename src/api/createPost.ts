446dc37a24164f972ba8c8be9e7302bb5633d6a3
Davi Patricio <davipatricio2006@gmail.com>
2023-02-18 18:12:59 +0000

    send json when creating a post

--- src/api/createPost.ts
+++ src/api/createPost.ts
@@ -1,18 +1,22 @@
 import { axios } from "./axios";
 
-const doRequest = (formData: FormData) => {
-  axios
-    .post("/posts", formData)
-    .then(console.log)
-    .catch(console.log);
+interface PostData {
+  content: string;
+  files: string[];
+}
+
+const doRequest = (finalData: PostData) => {
+  axios.post("/posts", finalData).then(console.log).catch(console.log);
 };
 
 export default function createPost(content: string, files: File[]) {
-  const formData = new FormData();
-  formData.append("content", content);
+  const finalData: PostData = {
+    content,
+    files: [],
+  };
 
   if (files.length === 0) {
-    doRequest(formData);
+    doRequest(finalData);
     return;
   }
 
@@ -22,9 +26,9 @@ export default function createPost(content: string, files: File[]) {
 
     reader.onload = () => {
       const base64 = reader.result?.toString();
-      if (base64) formData.append("files", base64);
+      if (base64) finalData.files.push(base64);
 
-      if (files.length === Number(index) + 1) doRequest(formData);
+      if (files.length === Number(index) + 1) doRequest(finalData);
     };
   }
 }
