18ea6f68cfc7c85a093bcbc1b310e5b70b3721bc
Davi Patricio <davipatricio2006@gmail.com>
2023-02-18 18:56:12 +0000

    minify class names on production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,46 @@
+const path = require("path");
+const loaderUtils = require("loader-utils");
+
+// https://stackoverflow.com/questions/66744765/how-to-hash-css-class-names-in-nextjs
+const hashOnlyIdent = (context, _, exportName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, "/")}#className:${exportName}`
+      ),
+      "md4",
+      "base64",
+      6
+    )
+    .replace(/[^a-zA-Z0-9-_]/g, "_")
+    .replace(/^(-?\d|--)/, "_$1");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
 
-module.exports = nextConfig
+  webpack(config, { dev }) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    if (!dev)
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes("css-loader") &&
+            !moduleLoader.loader?.includes("postcss-loader")
+          )
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+        });
+      });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
