7239f6422a95f27f39ef6c95c5ebf0a25ef29022
Davi Patricio <davipatricio2006@gmail.com>
2023-02-21 17:08:46 +0000

    more progress

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -5,20 +5,33 @@ export default function Profile({ params }: { params: { username: string } }) {
     <div className={styles.container}>
       <header className={styles.header}>
         <section className={styles.profileImages}>
-          {/* 1270x720 placeholder */}
           <img
             src="https://via.placeholder.com/500x220"
             alt="Profile banner"
             className={styles.banner}
+            draggable={false}
+            loading="lazy"
+            decoding="async"
           />
-          {/* 300x300 placeholder */}
+
           <img
             src="https://via.placeholder.com/120"
             alt="Profile avatar"
             className={styles.avatar}
+            draggable={false}
+            loading="lazy"
+            decoding="async"
           />
         </section>
       </header>
+
+      <section className={styles.content}>
+        <h1 className={styles.username}>{params.username}</h1>
+        <span>@{params.username}</span>
+        <p className={styles.bio}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        </p>
+      </section>
     </div>
   );
 }
