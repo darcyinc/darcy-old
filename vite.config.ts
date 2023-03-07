5a5f365886de740fe86da75168279495711f5f6e
Davi Patricio <davipatricio2006@gmail.com>
2023-03-07 01:42:14 +0000

    upgrad deps & remove vite-plugin-optimize-css-modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
-import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
 
 export default defineConfig({
-  plugins: [solid(), imagetools(), optimizeCssModules()],
+  plugins: [solid(), imagetools()],
 });
